e18e70a02d2560bd74fa8aa0e12c1e66e259b612
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-10-23 15:29:15 +0800

    Branch: lab03,add lab03's sample code.

---
+++ lab03/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eoux pipefail
+###
+ # @Github: https://github.com/Certseeds/CS1056
+ # @Organization: SUSTech
+ # @Author: nanoseeds
+ # @Date: 2020-10-23 15:17:43
+ # @LastEditors: nanoseeds
+ # @LastEditTime: 2020-10-23 15:18:11
+### 
+pip3 install fancyimpute
+# run this before init
